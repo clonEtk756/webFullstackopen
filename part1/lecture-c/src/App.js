df1598449adf9c1108cee242a3be6853469aaf2d
Egor <abezriad@student.ubc.ca>
2022-07-07 22:52:29 -0700

    Done with Lecture C

--- part1/lecture-c/src/App.js
+++ part1/lecture-c/src/App.js
@@ -1,22 +1,38 @@
-const Hello = (props) => {
-  return (
-    <div>
-      <p>
-        Hello {props.name}, you are {props.age} years old
-      </p>
-    </div>
-  )
-}
+import { useState } from 'react'
 
 const App = () => {
-  const name = 'Peter'
-  const age = 10
+  const [ counter, setCounter ] = useState(0)
+
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const resetToZero = () => setCounter(0)
+
+  const Display = ({ counter }) => <div>{counter}</div>
+
+  const Button = ({ onClick, text }) => {
+    return (
+      <button onClick={onClick}>
+        {text}
+      </button>
+    )
+  }
 
   return (
     <div>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={name} age={age} />
+      <Display counter={counter}/>
+
+    <Button
+      onClick={increaseByOne}
+      text='plus'
+    />
+    <Button
+      onClick={decreaseByOne}
+      text='minus'
+    />
+    <Button
+      onClick={resetToZero}
+      text='reset'
+    />
     </div>
   )
 }
